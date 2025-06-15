26e2b5e0d7a70cd6fd004f825707613395299fe0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-06-15 22:31:38 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -419,30 +419,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('✘ Cʟᴏꜱᴇ', 'close_data'),
             InlineKeyboardButton('« Bᴀᴄᴋ', 'help')           
         ]]
-
         if query.from_user.id not in ADMINS:
             return await query.answer("Sᴏʀʀʏ Tʜɪs Mᴇɴᴜ Oɴʟʏ Fᴏʀ Mʏ Aᴅᴍɪɴs ⚒️", show_alert=True)
-
         await query.message.edit("Pʀᴏᴄᴇꜱꜱɪɴɢ Wᴀɪᴛ Fᴏʀ 15 ꜱᴇᴄ...")
-
         total, used, free = shutil.disk_usage(".")
-        uptime = get_time(time.time() - client.start_time)
-
-        stats = script.SERVER_STATS.format(
-            uptime,
-            psutil.cpu_percent(),
-            psutil.virtual_memory().percent,
-            humanbytes(total),
-            humanbytes(used),
-            psutil.disk_usage('/').percent,
-            humanbytes(free)
-        )
-
+        uptime = temp.UPTIME
+        stats = script.SERVER_STATS.format(get_time(time.time() - uptime), psutil.cpu_percent(), psutil.virtual_memory().percent, humanbytes(total), humanbytes(used), psutil.disk_usage('/').percent, humanbytes(free))            
         stats_pic = await make_carbon(stats, True)
-        await query.edit_message_media(
-            InputMediaPhoto(stats_pic, script.ADMIN_TXT, enums.ParseMode.HTML),
-            reply_markup=InlineKeyboardMarkup(buttons)
-        )
+        await query.edit_message_media(InputMediaPhoto(stats_pic, script.ADMIN_TXT, enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
+        
     elif query.data == "openfilter":
         buttons = [[
             InlineKeyboardButton('AᴜᴛᴏFɪʟᴛᴇʀ', 'autofilter'),
