4c412302c9992e612f41436f88251a18f181a6a2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-06-22 22:11:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -139,13 +139,14 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         btn.append(
             [InlineKeyboardButton(text="❄️ ᴩᴀɢᴇꜱ 1/1", callback_data="pages")]
         )
-
+        
+    wait_msg = await message.reply_text("⏳ Hang tight, searching for you...")
+           
     if PM_IMDB:
         imdb = await get_poster(search)
     else:
         imdb = None
-    wait_msg = await message.reply_text("⏳ Hang tight, searching for you...")
-            
+     
     TEMPLATE = IMDB_TEMPLATE
     if imdb:
         cap = TEMPLATE.format(
